196afc1b1ad02adef216a6fedbf20b7e747e544f
Nikolai <nikolai.ahlhelm@gmail.com>
2025-10-22 13:37:39 +0200

    menu & settings
    Added core Godot project files, art assets, export configuration, platform tools, and initial scenes and scripts. Migrated main scene and script to layer-1, and introduced global audio and settings management.

---
+++ scripts/settings.gd
@@ -0,0 +1,27 @@
+extends Control
+
+signal settings_close
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	Settings.hide()
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+
+func _on_volume_slider_value_changed(value: float) -> void:
+	Log.debug("[SETTINGS] [_on_volume_slider_value_changed] volume slider changed to: " + str(value))
+	GlobalSettings.set_music_volume(%VolumeSlider.value)
+
+
+func _on_back_pressed() -> void:
+	self.hide()
+	Menu.show()
+
+func _on_save_pressed() -> void:
+	GlobalSettings.save_settings()
