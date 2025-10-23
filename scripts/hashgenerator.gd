097307f2d53d4005b966b9bae5d2d67a2e12bfa7
Niklas <niklas@dhein.de>
2025-10-23 11:29:57 +0200

    More updates

--- scripts/hashgenerator.gd
+++ scripts/hashgenerator.gd
@@ -5,7 +5,7 @@ var hash_dad
 var has_generated_hash = false
 
 # Load the godot icon
-var my_texture = load("res://images/Hash.png")
+var my_texture = load("res://images/square_blue.svg")
 
 func _ready() -> void:
 	hash_dad = $HashDad
