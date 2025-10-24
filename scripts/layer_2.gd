da4655a95b68f8647223773b198b49c29a4c1df2
Kristof Kulber <kristofkulber@gmail.com>
2025-10-24 11:58:34 +0200

    Added Layer 2 Frontend

--- scripts/layer_2.gd
+++ scripts/layer_2.gd
@@ -1,8 +1,32 @@
 extends Node2D
 
+const BLOCK = preload("res://scenes/layer2-block.tscn")
+
+const DISTANCE_BLOCKS = 470
+
+func _ready() -> void:
+	var amount_blocks = BlockchainNetwork.blockchain_machines[BlockchainNetwork.current_id]["blockchain"].size()
+	for block_id in range(amount_blocks - 1, -1, -1):
+		print(block_id)
+		var block = BLOCK.instantiate()
+		block.id = block_id
+		block.position = Vector2(-((amount_blocks - block_id - 1) * DISTANCE_BLOCKS), 0)
+		$"CanvasLayer/blockchain-container"/blockchain.add_child(block)
 
 func _on_button_pressed() -> void:
 	Globals.load_scene(Globals.layer1_scene)
 
 func _on_back_layer_2_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/layer-3.tscn")
+
+
+func _on_left() -> void:
+	$"CanvasLayer/blockchain-container".translate(Vector2(DISTANCE_BLOCKS, 0))
+	$AnimationPlayer.play("left")
+
+func _on_right() -> void:
+	$"CanvasLayer/blockchain-container".translate(Vector2(-DISTANCE_BLOCKS, 0))
+	$AnimationPlayer.play("right")
+
+func _new_block() -> void:
+	Globals.load_scene(Globals.layer1_scene)
